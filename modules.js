58e16e280558a1e55cca1bcbd0c8cefb18fc2548
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-07-24 17:20:40 +0200

    updating files

---
+++ modules.js
@@ -0,0 +1,8 @@
+const {people,ages} = require('./people');
+
+
+console.log(people,ages);
+
+const os = require('os');
+
+console.log(os.platform(),os.homedir());
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Abir','Mohammed','Oussama','ba3ziz','itachi'];
+const ages = [27,23,25,21,23];
+
+module.exports = {
+    people,
+    ages
+};
