283a7975ae5d9e7ebb801e30104847ec458072fc
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-08-01 06:15:52 +0200

    adding mongoose

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+const { result } = require('lodash');
+
 const app = express();
 
-app.set('view engine','ejs');
+const dbURI = 'mongodb+srv://mohammedbenotmane:1475963mimo@nodeblog.12e5a.mongodb.net/blog?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology:true})
+        .then((result)=> app.listen(3000))
+        .catch((err)=>console.log(err));
+
+app.set('view engine', 'ejs');
 
 
-app.listen(3000);
 
-app.use((req,res,next)=>{
-    console.log('new request made:');
-    console.log('host:',req.hostname);
-    console.log('path:',req.path);
-    console.log('method:',req.method);
-    next();
-});
 
-app.use((req,res,next)=>{
-    console.log('next middleware');
-    next();
-});
+app.use(morgan('dev'));
 
-app.get('/', (req,res)=>{
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
     const blogs = [
-        {title:"First Blog", snippet:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?"},
-    {title:"Second Blog", snippet:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?"},
-    {title:"third Blog", snippet:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?"},
+        { title: "First Blog", snippet: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?" },
+        { title: "Second Blog", snippet: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?" },
+        { title: "third Blog", snippet: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione impedit iure enim veritatis eaque excepturi! Velit veniam laboriosam impedit hic eius error placeat alias? Enim dolorum velit tempore a eligendi?" },
     ];
-    res.render('index',{title: 'Home',blogs: blogs});
+    res.render('index', { title: 'Home', blogs: blogs });
 });
 
-app.get('/about', (req,res)=>{
-    res.render('about',{title: 'About'});
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
 });
 
-app.get('/about-me', (req,res)=>{
+app.get('/about-me', (req, res) => {
     res.redirect('/about');
 });
 
-app.get('/blogs/create',(req,res)=>{
-    res.render('create',{title: 'Create a new Blog'});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
 });
 
-app.use((req,res)=>{
-    res.status(404).render('404',{title: '404'});
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
 });
