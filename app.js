f73b6432c709d83fb6d67b59c2b1e23957e3263f
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-08-01 02:33:44 +0200

    update project

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine','ejs');
+
+
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.sendFile('./views/index.html',{root: __dirname});
+    res.render('index',{title: 'Home'});
 });
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./views/about.html',{root: __dirname});
+    res.render('about');
 });
 
 app.get('/about-me', (req,res)=>{
     res.redirect('/about');
 });
 
+app.get('/blogs/create',(req,res)=>{
+    res.render('create');
+});
+
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root: __dirname});
+    res.status(404).render('404');
 });
